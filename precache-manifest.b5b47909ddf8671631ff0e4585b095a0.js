58fd0e4ed05d197a1a3c338422b6b9a2ac8c58dc
jeevitha <jeevithasakthi137@gmail.com>
2020-04-02 12:07:00 +0530

    project

---
+++ precache-manifest.b5b47909ddf8671631ff0e4585b095a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f885fd278df4f804dad",
+    "url": "/static/css/main.f26af0ca.chunk.css"
+  },
+  {
+    "revision": "7f885fd278df4f804dad",
+    "url": "/static/js/main.d505496e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "19116dc9d864625cf765",
+    "url": "/static/js/2.807c3918.chunk.js"
+  },
+  {
+    "revision": "fb5dd537181720f15f2fdb39fc757a1f",
+    "url": "/static/media/bg.fb5dd537.jpeg"
+  },
+  {
+    "revision": "772cedfdc3bf665df7e9fd762aaeebe1",
+    "url": "/index.html"
+  }
+];
